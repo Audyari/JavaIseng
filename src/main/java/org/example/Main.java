7c3a5d2d9ac3742ca35cabccb6539e1bb397de85
Audyari <audy123ari@gmail.com>
2026-07-01 11:43:25 +0700

    Initial commit - Clean slate

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
          System.out.println("Apaan SIh");
+         System.out.println("Apaan SIh");
+         System.out.println("Test AUdy");
     }
 }
